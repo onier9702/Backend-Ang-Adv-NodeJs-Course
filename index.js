0de5533887340be446e3d310b27d5ac27a78639a
onier9702 <onier0217@gmail.com>
2023-02-09 16:17:53 -0600

    Redirect Node Backend to another path that not belong to node like angular app path to index.html

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 
 const express = require('express');
 const cors = require('cors')
+const path = require('path');
 const { dbConnection } = require('./db/config');
 require('dotenv').config()
 
@@ -29,6 +30,10 @@ app.use( '/api/doctors', require('./routes/route_doctor') );
 app.use( '/api/all', require('./routes/route_search') );
 app.use( '/api/upload', require('./routes/route_upload') );
 
+// the last
+app.get('*', (req, res) => {
+    res.sendFile( path.resolve( __dirname, 'public/index.html' ) );
+})
 
 app.listen( process.env.PORT, () => {
     console.log(`App running on port:${process.env.PORT}`);
